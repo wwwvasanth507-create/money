486dc8b699710baabc1fc23102f66e1b4d32d3eb
wwwvasanth507-create <www.vasanth507@gmail.com>
2026-08-31 16:31:14 +0530

    seed_database_on_build

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run database setup & seeds if needed
-python -c "from app.database import engine, Base; from app.models import *; Base.metadata.create_all(bind=engine)"
+# Run database setup & seed default accounts
+python scripts/seed.py
+
